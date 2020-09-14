ab19e84bc693d2b4cd6be83f0661956c6355b16d
Biman Raj Shrestha <51041395+BimanShrestha@users.noreply.github.com>
2020-09-14 10:56:55 +0545

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CSHARPBASIC
+{
+    [DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hello enter your name?");
+            string name =Console.ReadLine();
+            Console.WriteLine("hi, "+ name);
+
+     Console.WriteLine("Hello World!");
+
+        }
+
+        private string GetDebuggerDisplay()
+        {
+            return ToString();
+        }
+    }
+}
